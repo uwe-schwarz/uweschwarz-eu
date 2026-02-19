d3ccd51a745cd91c389826653dd3d9f7d62bb765
Uwe <4044886+uwe-schwarz@users.noreply.github.com>
2026-02-19 21:57:18 +0100

    Address remaining safe nitpicks from PR review

--- src/components/ui/badge.tsx
+++ src/components/ui/badge.tsx
@@ -1,13 +1,14 @@
 "use client";
 
-import type React from "react";
+import React from "react";
 import { cn } from "@/lib/utils";
 import { badgeVariants, type BadgeVariantProps } from "./badge-variants";
 
 interface BadgeProps extends BadgeVariantProps, React.HTMLAttributes<HTMLDivElement> {}
 
-function Badge({ className, variant, ...props }: BadgeProps) {
-  return <div className={cn(badgeVariants({ variant }), className)} {...props} />;
-}
+const Badge = React.forwardRef<HTMLDivElement, BadgeProps>(({ className, variant, ...props }, ref) => {
+  return <div className={cn(badgeVariants({ variant }), className)} ref={ref} {...props} />;
+});
+Badge.displayName = "Badge";
 
 export { Badge };
--- src/components/ui/tooltip.tsx
+++ src/components/ui/tooltip.tsx
@@ -3,5 +3,6 @@
 import { Provider as TooltipPrimitiveProvider } from "@radix-ui/react-tooltip";
 
 const TooltipProvider = TooltipPrimitiveProvider;
+TooltipProvider.displayName = "TooltipProvider";
 
 export { TooltipProvider };
