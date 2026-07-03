9521e8725717f7d4c671f9951341d8e68a5b6ba5
Uwe <4044886+uwe-schwarz@users.noreply.github.com>
2026-07-03 03:07:45 +0000

    chore(deps): upgrade dependencies to latest

--- src/components/ui/toaster.tsx
+++ src/components/ui/toaster.tsx
@@ -10,7 +10,7 @@ export function Toaster() {
     <ToastProvider>
       {toasts.map(function ({ action, description, id, title, ...props }) {
         return (
-          <Toast key={id} {...props}>
+          <Toast {...props} key={id}>
             <div className="grid gap-1">
               {title && <ToastTitle>{title}</ToastTitle>}
               {description && <ToastDescription>{description}</ToastDescription>}
